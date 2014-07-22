ffc6142f3a4269d590c40751581dfde5126f0efd
root <root@bhawna-Satellite-C640.(none)>
2014-07-22 18:29:15 +0530

    first commit

---
+++ src/main/java/client/testclient.java
@@ -0,0 +1,13 @@
+package client;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+ 
+
+public class testclient {
+	
+	
+	
+	
+
+}
